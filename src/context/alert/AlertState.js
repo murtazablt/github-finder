670ea7ef35ca82ee9b120a3d7c6fb15ed620688a
Murtaza Bulut <bulut.2097@gmail.com>
2022-06-18 19:23:14 +0300

    some unused imports removed

--- src/context/alert/AlertState.js
+++ src/context/alert/AlertState.js
@@ -1,9 +1,9 @@
-import React, { useReducer } from 'react';
-import AlertContext from './alertContext';
-import AlertReducer from './alertReducer';
-import { SET_ALERT, REMOVE_ALERT } from '../types';
+import { useReducer } from "react";
+import AlertContext from "./alertContext";
+import AlertReducer from "./alertReducer";
+import { SET_ALERT, REMOVE_ALERT } from "../types";
 
-const AlertState = props => {
+const AlertState = (props) => {
   const initialState = null;
 
   const [state, dispatch] = useReducer(AlertReducer, initialState);
@@ -12,7 +12,7 @@ const AlertState = props => {
   const setAlert = (msg, type) => {
     dispatch({
       type: SET_ALERT,
-      payload: { msg, type }
+      payload: { msg, type },
     });
 
     setTimeout(() => dispatch({ type: REMOVE_ALERT }), 5000);
@@ -22,7 +22,7 @@ const AlertState = props => {
     <AlertContext.Provider
       value={{
         alert: state,
-        setAlert
+        setAlert,
       }}
     >
       {props.children}
